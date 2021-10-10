3dfa939bf62ba76b2b87ba6224450f9c1fbf82e1
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-10-10 21:14:48 +0800

    101021

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/views'));
+app.set('view engine','ejs');
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+})
+
+let port = process.env.PORT || 3000; 
+app.listen(port , ()=> console.log(`Listening to port ${port}`))
