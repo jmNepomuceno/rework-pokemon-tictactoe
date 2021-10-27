9506247a39a2710c85cde3d9e53fb4206ae1e89c
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-10-27 23:25:59 +0800

    102721

--- views/js/play.js
+++ views/js/play.js
@@ -5,6 +5,71 @@ const player_mini_icon = document.querySelectorAll('.mini-icon')
 const p1_health_bar = document.querySelector('.arena-div__p1-div__health .bar-div .health-div')
 const p2_health_bar = document.querySelector('.arena-div__p2-div__health .bar-div .health-div')
 
+const boxes_div = document.querySelectorAll('.boxes')
+let x_o = false
+
+const main = () =>{
+    for(let i = 0; i < boxes_div.length; i++) {
+        boxes_div[i].addEventListener('click', () => {clicked_box(i)})
+    }
+}
+
+const clicked_box = (index) => {
+    boxes_div[index].textContent = (!x_o) ? "X" : "O"
+    boxes_div[index].style.pointerEvents = "none"
+
+    check_pattern((!x_o) ? "X" : "O");
+    x_o = !x_o
+}
+
+const disable_box = () => {
+    for(let i = 0; i < boxes_div.length; i++) {
+        boxes_div[i].style.pointerEvents = "none"
+    }
+}
+
+const check_pattern = (ltr) => {
+
+
+    if(boxes_div[0].textContent == ltr && boxes_div[1].textContent == ltr && boxes_div[2].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+    else if(boxes_div[3].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[5].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+    else if(boxes_div[6].textContent == ltr && boxes_div[7].textContent == ltr && boxes_div[8].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+
+    else if(boxes_div[0].textContent == ltr && boxes_div[3].textContent == ltr && boxes_div[6].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+    else if(boxes_div[1].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[7].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+    else if(boxes_div[2].textContent == ltr && boxes_div[5].textContent == ltr && boxes_div[8].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+
+    else if(boxes_div[0].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[8].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+    else if(boxes_div[2].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[6].textContent == ltr){
+        health_bar(ltr);
+        disable_box();
+    }
+    else{
+        console.log("asdf")
+    }
+}
+
 for(let i = 0; i < 2; i++){
     if(chars_lbl[i].textContent == "Pikachu"){
         char_icon[i].src = "/css/imgs/index/pikachuMain.jpg"
@@ -31,23 +96,42 @@ for(let i = 0; i < 2; i++){
         player_mini_icon[i].src = "/css/imgs/arena/piplupIcon.png"
     }
 }
-// let i = 0
-// if (i == 0) {
 
-//       i = 1;
-//       var elem = p2_health_bar;
-//       var width = 100;
-//       var id = setInterval(frame, 100);
-//       function frame() {
-//         if (width <= 1) {
-//           clearInterval(id);
-//           i = 0;
-//         } else {
-//           width--;
-//           elem.style.width = width + "%";
-//         }
-//       }
-//     }
+const health_bar = (player) => {
+    let i = 0
+    let player_health_bar = (player == "X") ? p1_health_bar : p2_health_bar
+
+    if (i == 0) {
+
+        i = 1;
+        var elem = player_health_bar;
+
+        var width = player_health_bar.offsetWidth
+
+        let limit = width - 25
+
+        var id = setInterval(frame, 100);
+
+        function frame() {
+            if (width <= limit) {
+            clearInterval(id);
+            i = 0;
+            } else {
+            width--;
+            elem.style.width = width + "%";
+            }
+        }
+    }
+
+    // reset battle field
+    for(let i = 0; i < boxes_div.length; i++) {
+        boxes_div[i].textContent = ""
+        boxes_div[i].style.pointerEvents = "auto"
+        x_o = false
+
+    }
+}
+
 
 function move() {
     if (i == 0) {
@@ -66,3 +150,4 @@ function move() {
         }
       }
 }
+
