e6de395a88ae8e241c949a49dc179dc152080acd
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-10-28 20:13:17 +0800

    102821_b

--- views/js/play.js
+++ views/js/play.js
@@ -5,12 +5,15 @@ const player_mini_icon = document.querySelectorAll('.mini-icon')
 const p1_health_bar = document.querySelector('.arena-div__p1-div__health .bar-div .health-div')
 const p2_health_bar = document.querySelector('.arena-div__p2-div__health .bar-div .health-div')
 
+const arena_div = document.querySelector('arena-div__battle-div')
 const boxes_div = document.querySelectorAll('.boxes')
+const rows = document.querySelectorAll('.rows')
 let x_o = false
 
 let p1_health_bar_width = p1_health_bar.offsetWidth
 let p2_health_bar_width = p2_health_bar.offsetWidth
 let to_fix_width = 100 
+
 const main = () =>{
     for(let i = 0; i < boxes_div.length; i++) {
         boxes_div[i].addEventListener('click', () => {clicked_box(i)})
@@ -40,40 +43,58 @@ const enable_box = () => {
     x_o = true
 }
 
+const create_element = (where) => {
+    if(where){
+        let secondary_row = document.createElement('div') 
+        secondary_row.className = "sec_row"
+        arena_div.appendChild(secondary_row)
+    }else{
+        arena_div.removeChild(document.querySelector('.sec_row'))
+    }
+}
+
 const check_pattern = (ltr) => {
 
-
     if(boxes_div[0].textContent == ltr && boxes_div[1].textContent == ltr && boxes_div[2].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
     else if(boxes_div[3].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[5].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
     else if(boxes_div[6].textContent == ltr && boxes_div[7].textContent == ltr && boxes_div[8].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
 
     else if(boxes_div[0].textContent == ltr && boxes_div[3].textContent == ltr && boxes_div[6].textContent == ltr){
+        create_element(true);
+
         health_bar(ltr);
         disable_box();
     }
     else if(boxes_div[1].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[7].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
     else if(boxes_div[2].textContent == ltr && boxes_div[5].textContent == ltr && boxes_div[8].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
 
     else if(boxes_div[0].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[8].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
     else if(boxes_div[2].textContent == ltr && boxes_div[4].textContent == ltr && boxes_div[6].textContent == ltr){
+        create_element(true);
         health_bar(ltr);
         disable_box();
     }
@@ -140,7 +161,7 @@ const health_bar = (player) => {
                 }
                 to_fix_width = 0
                 enable_box();
-                
+                create_element(false)
             } else {
                 width--;  
                 elem.style.width = width + "%";
