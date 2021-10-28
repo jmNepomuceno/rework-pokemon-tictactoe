4cd0f9059ab2cdb0de0ed6f8f3a14a7211d6e218
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-10-28 21:07:25 +0800

    102821_c

--- views/js/play.js
+++ views/js/play.js
@@ -14,6 +14,320 @@ let p1_health_bar_width = p1_health_bar.offsetWidth
 let p2_health_bar_width = p2_health_bar.offsetWidth
 let to_fix_width = 100 
 
+const skillsX = {
+	"Pikachu" : {
+		h1 : {
+			top : "0",
+			left : "-5%",
+			deg : "0deg"
+		},
+		h2 : {
+			top : "31%",
+			left : "-5px",
+			deg : "0deg"
+		},
+		h3 : {
+			top : "65%",
+			left : "-5px",
+			deg : "0deg"
+		},
+
+		v1 : {
+			top : "30%",
+			left : "-31%",
+			deg : "90deg"
+		},
+		v2 : {
+			top : "30%",
+			left : "0",
+			deg : "90deg"
+		},
+		v3 : {
+			top : "30%",
+			left : "35%",
+			deg : "90deg"
+		},
+
+		sL : {
+			width : "130%",
+			top : "25%",
+			left : "-15%",
+			deg : "42deg"
+		},
+		sR : {
+			width : "140%",
+			top : "35%",
+			left : "-30%",
+			deg : "312deg"
+		}
+	},
+
+	1 : {
+		h1 : {
+			width : "450px",
+			top : "25px",
+			left : "0px",
+			deg : "0deg"
+		},
+		h2 : {
+			width : "450px",
+			top : "175px",
+			left : "0px",
+			deg : "0deg"
+		},
+		h3 : {
+			width : "450px",
+			top : "325px",
+			left : "0px",
+			deg : "0deg"
+		},
+
+		v1 : {
+			width : "463px",
+			top : "173px",
+			left : "-150px",
+			deg : "90deg"
+		},
+		v2 : {
+			width : "463px",
+			top : "173px",
+			left : "0px",
+			deg : "90deg"
+		},
+		v3 : {
+			width : "463px",
+			top : "173px",
+			left : "150px",
+			deg : "90deg"
+		},
+
+		sL : {
+			width : "550px",
+			top : "180px",
+			left : "-50px",
+			deg : "45deg"
+		},
+		sR : {
+			width : "550px",
+			top : "180px",
+			left : "-45px",
+			deg : "-45deg"
+		}
+	},
+
+	2 : {
+		h1 : {
+			width : "450px",
+			top : "25px",
+			left : "0px",
+			deg : "0deg"
+		},
+		h2 : {
+			width : "450px",
+			top : "175px",
+			left : "0px",
+			deg : "0deg"
+		},
+		h3 : {
+			width : "450px",
+			top : "325px",
+			left : "0px",
+			deg : "0deg"
+		},
+
+		v1 : {
+			width : "450px",
+			top : "173px",
+			left : "-150px",
+			deg : "90deg"
+		},
+		v2 : {
+			width : "450px",
+			top : "173px",
+			left : "0px",
+			deg : "90deg"
+		},
+		v3 : {
+			width : "450px",
+			top : "173px",
+			left : "150px",
+			deg : "90deg"
+		},
+
+		sL : {
+			width : "600px",
+			top : "160px",
+			left : "-65px",
+			deg : "45deg"
+		},
+		sR : {
+			width : "600px",
+			top : "165px",
+			left : "-85px",
+			deg : "-45deg"
+		}
+	},
+
+	3 : {
+		h1 : {
+			width : "450px",
+			top : "25px",
+			left : "0px",
+			deg : "-180deg"
+		},
+		h2 : {
+			width : "450px",
+			top : "175px",
+			left : "0px",
+			deg : "-180deg"
+		},
+		h3 : {
+			width : "450px",
+			top : "325px",
+			left : "0px",
+			deg : "-180deg"
+		},
+
+		v1 : {
+			width : "450px",
+			top : "175px",
+			left : "-150px",
+			deg : "-90deg"
+		},
+		v2 : {
+			width : "450px",
+			top : "175px",
+			left : "0px",
+			deg : "-90deg"
+		},
+		v3 : {
+			width : "450px",
+			top : "175px",
+			left : "150px",
+			deg : "-90deg"
+		},
+
+		sL : {
+			width : "600px",
+			top : "170px",
+			left : "-75px",
+			deg : "225deg"
+		},
+		sR : {
+			width : "600px",
+			top : "175px",
+			left : "-75px",
+			deg : "-225deg"
+		}
+	},
+
+	4 : {
+		h1 : {
+			width : "500px",
+			top : "20px",
+			left : "0px",
+			deg : "0deg"
+		},
+		h2 : {
+			width : "500px",
+			top : "170px",
+			left : "0px",
+			deg : "0deg"
+		},
+		h3 : {
+			width : "500px",
+			top : "320px",
+			left : "0px",
+			deg : "0deg"
+		},
+
+		v1 : {
+			width : "500px",
+			top : "200px",
+			left : "-170px",
+			deg : "90deg"
+		},
+		v2 : {
+			width : "500px",
+			top : "200px",
+			left : "-20px",
+			deg : "90deg"
+		},
+		v3 : {
+			width : "500px",
+			top : "200px",
+			left : "130px",
+			deg : "90deg"
+		},
+
+		sL : {
+			width : "670px",
+			top : "190px",
+			left : "-85px",
+			deg : "45deg"
+		},
+		sR : {
+			width : "670px",
+			top : "150px",
+			left : "-90px",
+			deg : "-45deg"
+		}
+	},
+
+	5 : {
+		h1 : {
+			width : "550px",
+			top : "20px",
+			left : "-5px",
+			deg : "-10deg"
+		},
+		h2 : {
+			width : "550px",
+			top : "175px",
+			left : "-5px",
+			deg : "-10deg"
+		},
+		h3 : {
+			width : "550px",
+			top : "330px",
+			left : "-5px",
+			deg : "-10deg"
+		},
+
+		v1 : {
+			width : "650px",
+			top : "270px",
+			left : "-255px",
+			deg : "82deg"
+		},
+		v2 : {
+			width : "650px",
+			top : "270px",
+			left : "-105px",
+			deg : "82deg"
+		},
+		v3 : {
+			width : "650px",
+			top : "270px",
+			left : "45px",
+			deg : "82deg"
+		},
+
+		sL : {
+			width : "800px",
+			top : "235px",
+			left : "-135px",
+			deg : "40deg"
+		},
+		sR : {
+			width : "800px",
+			top : "110px",
+			left : "-130px",
+			deg : "-55deg"
+		}
+	}
+}
+
 const main = () =>{
     for(let i = 0; i < boxes_div.length; i++) {
         boxes_div[i].addEventListener('click', () => {clicked_box(i)})
@@ -172,7 +486,6 @@ const health_bar = (player) => {
     
 }
 
-
 function move() {
     if (i == 0) {
         i = 1;
@@ -190,4 +503,5 @@ function move() {
         }
       }
 }
+
 
